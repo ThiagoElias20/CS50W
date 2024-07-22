40438d06acedd7a8323d849311fcd21543f584d0
ThiagoElias20 <contatothiagoelias22@gmail.com>
2024-07-22 16:56:07 -0300

    Starting the week2, Python

---
+++ Week2: Python/conditions.py
@@ -0,0 +1,7 @@
+number = int(input("Number: "))
+if number > 0:
+    print(f"{number} is positive!")
+elif number < 0:
+    print(f"{number} is negative!")
+else:
+    print(f"It's {number}") #If the number is zero
---
+++ Week2: Python/sequences.py
@@ -0,0 +1,9 @@
+# name = "Thiago"
+# print(name[0])
+
+# names = ["Thiago", "Elias", "Silva"]
+# print(names[1])
+
+# coordinateX = 10.0
+# coordinateY = 15.0
+coordinate = (10.0, 15.0) # This is a tuple!
